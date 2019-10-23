e3feeb3ef65dfb6a0c0fbe80348f426ab5c9b3e0
elisejakob <hei@elisejakob.no>
2019-10-23 15:17:25 +0200

    fix tracking script

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,19 +3,7 @@
     <Header />
     <nuxt />
     <Footer />
-    <script type="text/javascript">
-      var _paq = window._paq || [];
-      _paq.push(['disableCookies']);
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u="//a.444.no/";
-        _paq.push(['setTrackerUrl', u+'js/']);
-        _paq.push(['setSiteId', '4']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'js/'; s.parentNode.insertBefore(g,s);
-      })();
-    </script>
+    <div v-html="script"></div>
   </div>
 </template>
 
@@ -27,6 +15,24 @@ export default {
   components: {
     Header,
     Footer
+  },
+  data: function() {
+    return {
+      script: `<script type="text/javascript">
+        var _paq = window._paq || [];
+        _paq.push(['disableCookies']);
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="//a.444.no/";
+          _paq.push(['setTrackerUrl', u+'js/']);
+          _paq.push(['setSiteId', '4']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'js/'; s.parentNode.insertBefore(g,s);
+        })();
+      <\/script>
+    `
+    }
   }
 }
 </script>
