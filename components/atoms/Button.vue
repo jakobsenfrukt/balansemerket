af5e06fe647984b2856e7b4d15e400badf62b08f
elisejakob <hei@elisejakob.no>
2021-02-08 17:26:30 +0100

    samtalekort progress, start adding filters, buttons

---
+++ components/atoms/Button.vue
@@ -0,0 +1,41 @@
+<template>
+  <button class="button">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+    display: block;
+    width: 80%;
+    max-width: 500px;
+    background: #000;
+    color: #fff;
+    font-family: inherit;
+    font-size: 1.2rem;
+    text-decoration: none;
+    padding: 1.2rem 2rem;
+    margin: 1.5rem auto;
+    cursor: pointer;
+    border: none;
+    border-radius: 2rem;
+    transition: all .2s ease;
+    + .button {
+        margin-top: 0;
+    }
+
+    &:hover {
+        color: #fff;
+        transform: scale(1.05);
+    }
+}
+</style>
+
--- pages/samtalekort/index.vue
+++ pages/samtalekort/index.vue
@@ -1,11 +1,11 @@
 <template>
   <main class="site-main">
     <h1 class="site-title">Samtalekort</h1>
-    <div @click="readMore('instructions')" class="read-more show-instructions">Vis instruksjoner</div>
+    <div @click="readMore('instructions')" class="read-more show-instructions">Hvordan bruke kortene</div>
     <section class="page samtalekort">
       <div class="intro">
         <div class="content" v-html="cardIndex.introText.content"></div>
-        <button>Start</button>
+        <button>Trekk et kort</button>
         <button>Tilpass kortstokken</button>
       </div>
       <div class="card current">
@@ -14,18 +14,51 @@
       <nav class="card-nav">
         <button @click="cardNumber--" v-if="cardNumber > 0">Forrige kort</button>
         <button @click="cardNumber++"  v-if="cardNumber < selectedCards.length-1">Neste kort</button>
+        <button @click="showEnd()"  v-if="cardNumber === selectedCards.length-1">Avslutt</button>
+        <div class="card-counter">
+          {{cardNumber+1}} / {{ selectedCards.length }}
+        </div>
       </nav>
-      <div class="cards">
-        <div class="card" v-for="(card, index) in cards" :key="`card-${index}`">
-          {{card.title}} - {{card.category[0].title}}
+    </section>
+    <div class="customize" id="customize">
+      <h2>Tilpass kortstokken</h2>
+      <div class="customize-options customize-type">
+        <span>Vis kort etter type:</span>
+        <div>
+          <input type="checkbox" id="assertions" name="assertions" checked>
+          <label for="assertions">Påstander</label>
+        </div>
+        <div>
+          <input type="checkbox" id="questions" name="questions" checked>
+          <label for="questions">Refleksjonsspørsmål</label>
         </div>
       </div>
-      <div class="end">
-        <div class="content" v-html="cardIndex.endText.content"></div>
+      <div class="customize-options customize-category">
+        <span>Vis kort etter tema:</span>
+        <div>
+          <input type="checkbox" id="category1" name="category1" checked>
+          <label for="category1">Forebygge seksuell trakassering</label>
+        </div>
+        <div>
+          <input type="checkbox" id="category2" name="category2" checked>
+          <label for="category2">Utfordre normer</label>
+        </div>
+        <div>
+          <input type="checkbox" id="category3" name="category3" checked>
+          <label for="category3">Motvirke hersketeknikker</label>
+        </div>
       </div>
-    </section>
+    </div>
+    <div class="cards">
+      <div class="card" v-for="(card, index) in cards" :key="`card-${index}`">
+        {{card.title}} - {{card.category[0].title}}
+      </div>
+    </div>
     <div class="instructions" id="instructions">
       <div class="content" v-html="cardIndex.body.content"></div>
+    </div>
+    <div class="end">
+      <div class="content" v-html="cardIndex.endText.content"></div>
     </div>
   </main>
 </template>
@@ -115,7 +148,7 @@ export default {
 .card {
   display: inline-block;
   background: white;
-  border-radius: 6px;
+  border-radius: 2rem;
   box-shadow:
     -2px 2px 0 1px rgb(212, 212, 212),
     -.8rem .8rem 0 rgba(0, 0, 0, .2);
@@ -126,10 +159,10 @@ export default {
   &.current {
     display: block;
     width: 90%;
-    max-width: 900px;
+    max-width: 800px;
     margin: 2rem auto;
     font-size: 2rem;
-    padding: 3rem 2rem;
+    padding: 4rem 4.2rem;
   }
 }
 .cards {
@@ -143,6 +176,22 @@ export default {
 .read-more {
   cursor: pointer;
 }
+.customize {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  border-top: 2px solid black;
+  padding: 2rem 0;
+
+  h2 {
+    grid-column: span 3;
+    text-align: left;
+    width: 100%;
+    max-width: none;
+  }
+  span {
+    font-weight: 700;
+  }
+}
 .show-instructions {
   position: absolute;
   top: 2rem;
@@ -153,6 +202,8 @@ export default {
 .instructions {
   display: none;
   position: absolute;
+  top: 3rem;
+  right: 0;
   background: white;
 
   &.visible {
