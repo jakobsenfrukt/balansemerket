123285f0c94433f1f21726610104e4ca0f985e69
elisejakob <hei@elisejakob.no>
2021-04-13 18:53:06 +0200

    add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="site-main">
+    <h1>404: Finner ikke siden!</h1>
+    <section class="page error">
+    </section>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
